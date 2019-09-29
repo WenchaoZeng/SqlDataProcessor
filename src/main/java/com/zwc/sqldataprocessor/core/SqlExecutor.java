47f2c8c2cf232430229f3990525d6004c44ea8a9
Reid Zeng <wenchao@yit.com>
2019-09-29 18:00:30 +0800

    支持执行sql

--- src/main/java/com/zwc/sqldataprocessor/core/SqlExecutor.java
+++ src/main/java/com/zwc/sqldataprocessor/core/SqlExecutor.java
@@ -1,15 +1,170 @@
 package com.zwc.sqldataprocessor.core;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 import com.zwc.sqldataprocessor.core.entity.DataList;
+import com.zwc.sqldataprocessor.core.entity.DataList.ColumnType;
 import com.zwc.sqldataprocessor.core.entity.DatabaseConfig;
+import org.apache.commons.lang3.StringUtils;
 
 public class SqlExecutor {
 
     public static DataList exec(String sql, String databaseName, List<DatabaseConfig> configList, Map<String, DataList> tables) {
-        // TODO
-        return new DataList();
+
+        String rawSql = renderSql(sql, tables);
+
+        DatabaseConfig dbConfig = configList.stream().filter(x -> x.name.equals(databaseName)).findAny().orElse(null);
+        if (dbConfig == null) {
+            throw new RuntimeException(databaseName +"数据库的配置不存在");
+        }
+
+        return execRawSql(rawSql, dbConfig);
     }
+
+    static DataList execRawSql(String sql, DatabaseConfig dbConfig) {
+        String url = String.format("jdbc:mysql://%s:3306/%s?useUnicode=true&characterset=utf-8", dbConfig.dbHost, dbConfig.dbName);
+        try (Connection conn = DriverManager.getConnection(url, dbConfig.dbUserName, dbConfig.dbPassword)) {
+
+            // 解除group_concat的长度限制
+            try (Statement cmd = conn.createStatement()) {
+                cmd.execute("SET SESSION group_concat_max_len = 1000000;");
+            }
+
+            try (PreparedStatement cmd = conn.prepareStatement(sql, ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY)) {
+                cmd.setFetchSize(Integer.MIN_VALUE);
+                try (ResultSet resultSet = cmd.executeQuery()) {
+                    DataList table = new DataList();
+                    table.columns = new ArrayList<>();
+                    table.columnTypes = new ArrayList<>();
+
+                    // 列头
+                    ResultSetMetaData metaData = resultSet.getMetaData();
+                    int count = metaData.getColumnCount();
+                    for (int i = 1; i <= count; ++i) {
+                        String name = metaData.getColumnLabel(i);
+                        String type = metaData.getColumnTypeName(i);
+                        table.columns.add(name);
+                        if (type.equals("INT")) {
+                            table.columnTypes.add(ColumnType.INT);
+                        } else if (type.equals("DECIMAL")) {
+                            table.columnTypes.add(ColumnType.DECIMAL);
+                        } else if (type.equals("DATETIME")) {
+                            table.columnTypes.add(ColumnType.DATETIME);
+                        } else {
+                            table.columnTypes.add(ColumnType.TEXT);
+                        }
+                    }
+
+                    // 行数据
+                    while (resultSet.next()) {
+                        List<String> values = new ArrayList<>();
+                        for (int i = 1; i <= table.columns.size(); ++ i) {
+                            String value = resultSet.getString(i);
+                            values.add(value);
+                        }
+                        table.rows.add(values);
+                    }
+
+                    return table;
+                }
+            }
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    static String renderSql(String sql, Map<String, DataList> tables) {
+
+        for (String tableName : tables.keySet()) {
+            if (sql.contains("$" + tableName)) {
+                DataList table = tables.get(tableName);
+                if (table == null) {
+                    throw new RuntimeException("结果集$" + tableName + "不存在.");
+                }
+                StringBuilder builder = new StringBuilder();
+                builder.append("(\n");
+                String tableSelectSql = renderSelectSql(table);
+                builder.append(tableSelectSql);
+                builder.append(")");
+                sql = sql.replace("$" + tableName, builder.toString());
+            }
+        }
+
+        return sql;
+    }
+
+    static String renderSelectSql(DataList table) {
+        if (table.rows.size() <= 0) {
+            return renderEmptySelectSql(table);
+        }
+
+        StringBuilder builder = new StringBuilder();
+        for (int rowIndex = 0; rowIndex < table.rows.size(); ++rowIndex) {
+            String selectSql = renderSelectSql(rowIndex, table);
+            builder.append(selectSql);
+            builder.append("\n");
+            if (rowIndex < table.rows.size() - 1) {
+                builder.append(" union all \n");
+            }
+        }
+
+        return builder.toString();
+    }
+
+    static String renderEmptySelectSql(DataList table) {
+        List<String> rowValues = new ArrayList<>();
+        for (ColumnType type : table.columnTypes) {
+            if (type == ColumnType.INT || type == ColumnType.DECIMAL) {
+                rowValues.add("1");
+            } else if (type == ColumnType.DATETIME) {
+                rowValues.add("2019-02-15");
+            } else {
+                rowValues.add("");
+            }
+        }
+
+        return renderSelectSql(rowValues, table) + " where false";
+    }
+
+    static String renderSelectSql(int rowIndex, DataList table) {
+        return renderSelectSql(table.rows.get(rowIndex), table);
+    }
+
+    static String renderSelectSql(List<String> rowValues, DataList table) {
+        List<String> selectColumns = new ArrayList<>();
+        for (int columnIndex = 0; columnIndex < table.columns.size(); ++columnIndex) {
+            ColumnType type = table.columnTypes.get(columnIndex);
+            String name = table.columns.get(columnIndex);
+            String value = rowValues.get(columnIndex);
+            if (StringUtils.isBlank(value)) {
+                value = null;
+            }
+            String selectColumnFormat = null;
+            if (type == ColumnType.INT || type == ColumnType.DECIMAL) {
+                selectColumnFormat = "%s as `%s`";
+            } else if (type == ColumnType.DATETIME) {
+                selectColumnFormat = "cast('%s' as datetime) as `%s`";
+            } else {
+                if (value == null) {
+                    value = "";
+                }
+                value = value.replace("'", "\\'");
+                selectColumnFormat = "'%s' as `%s`";
+            }
+            selectColumns.add(String.format(selectColumnFormat, value, name));
+        }
+        return "select " + String.join(", ", selectColumns);
+    }
+
 }
