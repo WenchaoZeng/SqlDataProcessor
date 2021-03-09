85807facf24bcc52046d7fb12cb6216befa332da
Wenchao Zeng <zwc1986@gmail.com>
2021-03-09 19:40:21 +0800

    支持直接用start.sh命令启动, 不用切换到这个目录

--- package.sh
+++ package.sh
@@ -7,9 +7,6 @@ mkdir ./target/release
 mkdir ./target/release/lib
 cp ./target/sqldataprocessor-0.0.0-SNAPSHOT/lib/* ./target/release/lib/
 cp ./start.sh ./target/release
-cp ./start.sh ./target/release/start.bat
 
 # 打包
 zip -r9 ./target/release/java_SqlDataProcessor.zip ./target/release/*
-
-exit 0
--- start.sh
+++ start.sh
@@ -1,1 +1,5 @@
-java -XX:MinHeapFreeRatio=0 -XX:MaxHeapFreeRatio=10 -cp "lib/*" com.zwc.sqldataprocessor.SqlDataProcessor
+oldPath=$(pwd)
+currentDir=$(dirname $0)
+cd $currentDir
+java -XX:MinHeapFreeRatio=0 -XX:MaxHeapFreeRatio=10 -cp "lib/*" com.zwc.sqldataprocessor.SqlDataProcessor $1
+cd $oldPath
