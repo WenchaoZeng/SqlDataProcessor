45adb160e7a58c3566b3ad1d284bf17313fed36d
Wenchao Zeng <zwc1986@gmail.com>
2022-03-30 14:52:47 +0800

    内存限制改成16g

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 oldPath=$(pwd)
 currentDir=$(dirname $0)
 cd $currentDir
-java -XX:MinHeapFreeRatio=0 -XX:MaxHeapFreeRatio=10 -cp "lib/*" com.zwc.sqldataprocessor.SqlDataProcessor $1
+java -Xmx16g -XX:MinHeapFreeRatio=0 -XX:MaxHeapFreeRatio=10 -cp "lib/*" com.zwc.sqldataprocessor.SqlDataProcessor $1
 cd $oldPath
